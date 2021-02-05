de5c619d79d089d957c29de51a2756c405458e02
Chaewony <tcd1205@naver.com>
2021-02-06 01:04:55 +0900

    1/5

---
+++ Save/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform playerPos;
+
+    //경계값
+    public float minX;
+    public float minY;
+
+    public float maxX;
+    public float maxY;
+
+    //플레이어에 대한 카메라의 상대적인 포지션 값(기본 설정에서 카메라 포지션에서 플레이어의 포지션을 뺀 값)
+    public float relativePosX;
+    public float relativePosY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Clamp(playerPos.position.x + relativePosX, minX, maxX);
+        float y = Mathf.Clamp(playerPos.position.y + relativePosY, minY, maxY);
+        this.transform.position = new Vector3(x, y, this.transform.position.z);
+    }
+}
